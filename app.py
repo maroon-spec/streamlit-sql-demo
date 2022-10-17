e45799c4b9c888c8d42357b5ae5482e37d63fab1
maroon-spec <gont.jun@gmail.com>
2022-10-17 18:14:41 +0900

    f

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ rows = run_query("select Origin, count(*) from small_table group by Origin")
 # Print results.
 st.dataframe(rows)
 
-st.bar_chart(rows)
+st.bar_chart(data=rows, x=None, y=None, width=0, height=0, use_container_width=True)
