c2ae13c8966615d6bf961736792ca9219090c24b
maroon-spec <gont.jun@gmail.com>
2022-10-17 18:28:16 +0900

    a

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def run_query(query):
         return cur.fetchall()
 
 rows = run_query("select Origin, count(*) as count from small_table group by Origin")
-df = pd.DataFrame(rows, column=["Origin","count"])
+df = pd.DataFrame(rows, columns=["Origin","count"])
 
 # Print results.
 st.dataframe(df)
